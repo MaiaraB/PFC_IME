bdb55109598e3e0ea19026c8e0d670f6256dde0d
Arthur Araujo <arthur05@mit.edu>
2015-08-11 22:54:36 -0300

    Added specs for Service and created ServiceBuilder.

--- src/java/br/eb/ime/pfc/domain/Service.java
+++ src/java/br/eb/ime/pfc/domain/Service.java
@@ -4,31 +4,55 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Represents a service that contains information about all the users, 
- * all layers and all access levels used by the system.
+ * Represents a Service that contains information about all the Users, 
+ * all WMS Layers and all Access Levels used by the system.
+ * 
+ * One can use this class to retrieve Layers by its wmsId's, Users by its usernames,
+ * and AccessLevels by its names.
+ * 
+ * There are no Layers with the same wmsId's, no Users with the same username and
+ * no AccessLevels with the same names.
+ * 
+ * This class cannot be directly instantiated. To create a Service see ServiceBuilder 
+ * documentation for details.
  * 
  */
 public class Service {
     private final Map<String,User> users;
     private final Map<String,Layer> layers;
-    private final Map<String,AccessLevel> accessLevel;
+    private final Map<String,AccessLevel> accessLevels;
+    
+    /*
+    * Abstraction Function:
+    * users: maps usernames to User objects.
+    * layers: maps wmsId to Layer objects.
+    * accessLevel: maps names to AccessLevel objects.
+    *
+    * Representation Invariant:
+    * 
+    */
     
     /**
-     * Creates an instance of a service.
+     * 
+     * @param users
+     * @param layers
+     * @param accessLevels 
      */
-    public Service(){
-        users = new HashMap<>();
-        layers = new HashMap<>();
-        accessLevel = new HashMap<>();
+    private Service(Map<String,User> users,Map<String,Layer> layers,Map<String,AccessLevel> accessLevels){
+        this.users = users;
+        this.layers = layers;
+        this.accessLevels = accessLevels;
         checkRep();
     }
     
     /**
      * Checks the representation invariant.
+     * @return true if the representation invariant is maintained
      */
     private void checkRep(){
-        
+       
     }
+    
     
     //OBSERVERS
     
@@ -39,11 +63,17 @@ public class Service {
      * Requires: layerWmsId not empty.
      * @return the Layer specified in this Service that contains the wmsId with the
      * value specified by the param.
-     * @throws br.eb.ime.pfc.domain.Service.LayerNotFoundException if no Layers of this service has the specified
+     * @throws br.eb.ime.pfc.domain.ServiceElementNotFoundException if no Layers of this service have the specified
      * wmsId.
      */
-    public Layer getLayerById(String layerWmsId) throws LayerNotFoundException{
-        throw new RuntimeException();
+    public Layer getLayerById(String layerWmsId) throws ServiceElementNotFoundException{
+        final Layer layer = layers.get(layerWmsId);
+        if(layer == null){
+            throw new ServiceElementNotFoundException("The Layer with specified wmsId was not found in the Service");
+        }
+        else{
+            return layer;
+        }
     }
     
     /**
@@ -52,11 +82,17 @@ public class Service {
      * The name of the access level
      * @return the AccessLevel specified in this service that has the specified
      * name.
-     * @throws br.eb.ime.pfc.domain.Service.AccessLevelNotFoundException if no AccessLevel of this service has
+     * @throws br.eb.ime.pfc.domain.ServiceElementNotFoundException if no AccessLevel of this service has
      * the specified name.
      */
-    public AccessLevel getAccessLevelByName(String accessLevelName) throws AccessLevelNotFoundException{
-        throw new RuntimeException();
+    public AccessLevel getAccessLevelByName(String accessLevelName) throws ServiceElementNotFoundException{
+        final AccessLevel accessLevel = accessLevels.get(accessLevelName);
+        if(accessLevel == null){
+            throw new ServiceElementNotFoundException("The AccessLevel with the specified name was not found in the Service.");
+        }
+        else{
+            return accessLevel;
+        }
     }
     
     /**
@@ -64,159 +100,97 @@ public class Service {
      * @param username
      * The username of the User
      * @return The User of this service that has the same username specified
-     * @throws br.eb.ime.pfc.domain.Service.UserNotFoundException if no User of this service has the specified
+     * @throws br.eb.ime.pfc.domain.ServiceElementNotFoundException if no User of this service has the specified
      * username.
      */
-    public User getUserByUsername(String username) throws UserNotFoundException{
-        throw new RuntimeException();
-    }
-    
-    //MUTATORS
-    
-    /**
-     * Add a Layer to this Service.
-     * @param layer
-     * The Layer that will be added to this Service.
-     * Requires: The layer must not contain the same wmsId of a Layer already
-     * present in this service.
-     * @throws br.eb.ime.pfc.domain.Service.LayerRepetitionException 
-     */
-    public void addLayer(Layer layer) throws LayerRepetitionException{
-        checkRep();
-    }
-    
-    /**
-     * Add an AccessLevel to this Service.
-     * @param accessLevel
-     * The AccessLevel that will be added to this Service.
-     * Requires: The accessLevel must not contain the same name of an AccessLevel
-     * already present in this service.
-     * @throws br.eb.ime.pfc.domain.Service.AccessLevelRepetitionException 
-     */
-    public void addAccessLevel(AccessLevel accessLevel) throws AccessLevelRepetitionException{
-        checkRep();
-    }
-    
-    /**
-     * Add an User to this Service.
-     * @param user
-     * The User that will be added to this service.
-     * Requires: The user must not contain the same username of an User already present 
-     * in this service.
-     * @throws br.eb.ime.pfc.domain.Service.UserRepetitionException  if user has
-     * the same username of a User already present in this service.
-     */
-    public void addUser(User user) throws UserRepetitionException{
-        checkRep();
-    }
-    
-    /**
-     * This exception is triggered when no Layer is found in this service 
-     * with the specified parameters.
-     */
-    public class LayerNotFoundException extends RuntimeException{
-        
-        private static final long serialVersionUID = 1L;
-        
-        /**
-         * Creates a LayerNotFoundException with a detail message.
-         * @param message 
-         * The message that specifies the error.
-         */
-        public LayerNotFoundException(String message){
-            super(message);
+    public User getUserByUsername(String username) throws ServiceElementNotFoundException{
+        final User user = users.get(username);
+        if(user == null){
+            throw new ServiceElementNotFoundException("The User with the specified username was not found in the Service.");
         }
-        
-    }
-    
-    /**
-     * This exception is triggered when no AccessLevel is found in this service 
-     * with the specified parameters.
-     */
-    public class AccessLevelNotFoundException extends RuntimeException{
-        
-        private static final long serialVersionUID = 1L;
-        
-        /**
-         * Creates an AccessLevelNotFoundException with a detail message.
-         * @param message 
-         * The message that specifies the error.
-         */
-        public AccessLevelNotFoundException(String message){
-            super(message);
-        }
-        
-    }
-    
-    /**
-     * This exception is triggered when no User is found in this service 
-     * with the specified parameters.
-     */
-    public class UserNotFoundException extends RuntimeException{
-        
-        private static final long serialVersionUID = 1L;
-        
-        /**
-         * Creates a UserNotFoundException with a detail message.
-         * @param message 
-         * The message that specifies the error.
-         */
-        public UserNotFoundException(String message){
-            super(message);
-        }
-        
-    }
-    
-    /**
-     * This exception is triggered when one tries to add a Layer wich has a wmsId that is
-     * already present in a Layer of this service.
-     */
-    public class LayerRepetitionException extends RuntimeException{
-        private static final long serialVersionUID = 1L;
-        
-        /**
-         * Creates a LayerRepetitionException with a detail message.
-         * @param message 
-         * The message that specifies the error.
-         */
-        public LayerRepetitionException(String message){
-            super(message);
+        else{
+            return user;
         }
     }
     
     /**
-     * This exception is triggered when one tries to add an AccessLevel with the same name of
-     * an AccessLevel already present in this service.
+     * 
      */
-    public class AccessLevelRepetitionException extends RuntimeException{
+    public static class ServiceBuilder{
         
-        private static final long serialVersionUID = 1L;
+        private final Map<String,User> users;
+        private final Map<String,Layer> layers;
+        private final Map<String,AccessLevel> accessLevels;
         
         /**
-         * Creates an AccessLevelRepetitionException with a detail message.
-         * @param message 
-         * The message that specifies the error.
+         * 
          */
-        public AccessLevelRepetitionException(String message){
-            super(message);
+        public ServiceBuilder(){
+            users = new HashMap<>();
+            layers = new HashMap<>();
+            accessLevels = new HashMap<>();
         }
-    }
-    
-    /**
-     * This exception is triggered when one tries to add an User with the same username of 
-     * a User already present in this service.
-     */
-    public class UserRepetitionException extends RuntimeException{
         
-        private static final long serialVersionUID = 1L;
+         /**
+         * Add a Layer to the Service.
+         * @param layer
+         * The Layer that will be added to the Service.
+         * Requires: The layer must not contain the same wmsId of a Layer already
+         * present in the service.
+         * @throws br.eb.ime.pfc.domain.ServiceElementRepetitionException if
+         * the wmsId of layer is used by a Layer already  present in the Service.
+         */
+        public void addLayer(Layer layer) throws ServiceElementRepetitionException{
+            if(layers.containsKey(layer.getWmsId())){
+                throw new ServiceElementRepetitionException("There is a layer in the Service with the same wmsId");
+            }
+            else{
+                layers.put(layer.getWmsId(), layer);
+            }
+        }
+
+        /**
+         * Add an AccessLevel to the Service.
+         * @param accessLevel
+         * The AccessLevel that will be added to the Service.
+         * Requires: The accessLevel must not contain the same name of an AccessLevel
+         * already present in this service.
+         * @throws br.eb.ime.pfc.domain.ServiceElementRepetitionException if 
+         * the name of accessLevel is used by an AccessLevel already present in the Service.
+         */
+        public void addAccessLevel(AccessLevel accessLevel) throws ServiceElementRepetitionException{
+            if(accessLevels.containsKey(accessLevel.getName())){
+                throw new ServiceElementRepetitionException("There is an AccessLevel in the Service with the same name.");
+            }
+            else{
+                accessLevels.put(accessLevel.getName(), accessLevel);
+            }
+        }
+
+        /**
+         * Add an User to the Service.
+         * @param user
+         * The User that will be added to the service.
+         * Requires: The user must not contain the same username of an User already present 
+         * in this service.
+         * @throws br.eb.ime.pfc.domain.ServiceElementRepetitionException   if
+         * the username of user is already used by another User present in the Service.
+         */
+        public void addUser(User user) throws ServiceElementRepetitionException{
+            if(users.containsKey(user.getUsername())){
+                throw new ServiceElementRepetitionException("There is an User in the Service with the same username.");
+            }
+            else{
+                users.put(user.getUsername(), user);
+            }
+        }
         
         /**
-         * Creates an UserRepetitionException with a detail message.
-         * @param message 
-         * The message that specifies the error.
+         * 
+         * @return 
          */
-        public UserRepetitionException(String message){
-            super(message);
+        public Service buildService(){
+            throw new RuntimeException();
         }
     }
 }
