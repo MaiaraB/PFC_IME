f2ee473fffca41b4b3319ab2c4118ab9f1581d93
Arthur Araujo <arthur05@mit.edu>
2015-08-18 20:31:47 -0300

    Created Manager Classes for Layer, AccessLevel and User. The Manager classes are LayerManager, AccessLevelManager and UserManager and are used to perform CRUD operations to the database.

---
+++ src/java/br/eb/ime/pfc/domain/AccessLevelManager.java
@@ -0,0 +1,55 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 arthurfernandes.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package br.eb.ime.pfc.domain;
+
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author arthurfernandes
+ */
+public class AccessLevelManager {
+    private final SessionFactory sessionFactory;
+    
+    public AccessLevelManager(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+    
+    public void create(AccessLevel accessLevel){
+        this.sessionFactory.getCurrentSession().save(accessLevel);
+    }
+    
+    public AccessLevel getAccessLevel(String name){
+        return (AccessLevel) this.sessionFactory.getCurrentSession().get(AccessLevel.class, name);
+    }
+    
+    public void update(AccessLevel accessLevel){
+        this.sessionFactory.getCurrentSession().update(accessLevel);
+    }
+    
+    public void delete(AccessLevel accessLevel){
+        this.sessionFactory.getCurrentSession().delete(accessLevel);
+    }
+    
+}
---
+++ src/java/br/eb/ime/pfc/domain/LayerManager.java
@@ -0,0 +1,55 @@
+package br.eb.ime.pfc.domain;
+
+import org.hibernate.SessionFactory;
+
+/*
+ * The MIT License
+ *
+ * Copyright 2015 arthurfernandes.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ *
+ * @author arthurfernandes
+ */
+public class LayerManager {
+    private final SessionFactory sessionFactory;
+    
+    public LayerManager(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+    
+    public void create(Layer layer){
+        this.sessionFactory.getCurrentSession().save(layer);
+    }
+    
+    public Layer getLayerById(String wmsId){
+        return (Layer) this.sessionFactory.getCurrentSession().get(Layer.class, wmsId);
+    }
+    
+    public void update(Layer layer){
+        this.sessionFactory.getCurrentSession().update(layer);
+    }
+    
+    public void delete(Layer layer){
+        this.sessionFactory.getCurrentSession().delete(layer);
+    }
+}
